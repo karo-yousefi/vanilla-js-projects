36b6ac535f4433c8e972431c77e434cb73b93128
Karo Yousefi <88065530+karo-yousefi@users.noreply.github.com>
2024-10-12 16:07:20 +0330

    Delete projects/3-accordion-menu directory

--- projects/3-accordion-menu/src/script.js
+++
@@ -1,8 +0,0 @@
-const titleSectiopn = document.querySelectorAll(".title-section");
-const collapsible = document.querySelectorAll(".collapsible");
-
-titleSectiopn.forEach((e, i) => {
-  e.addEventListener("click",() => {
-    collapsible[i].classList.toggle("collapse");
-  });
-});
